1e3f5d1780ee7edf4fb1e8004a5e0c087ec29e61
Bang <15622356989@163.com>
2022-07-11 14:52:18 +0800

    fix: 删除多余文件

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,10 @@ import 'virtual:svg-icons-register';
 import '@/shared/styles/base.scss';
 
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 import App from './App.vue';
-// import { router } from './router';
 import SvgIcon from '@/components/SvgIcon.vue';
 
 const app = createApp(App);
-
-// app.use(router);
-app.use(createPinia());
 
 app.component('SvgIcon', SvgIcon);
 
